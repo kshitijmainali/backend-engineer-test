8511cb1110b3030cd9689b1daf2ed33450bfbe3f
kshitij <mainalikshitij>
2025-10-31 22:38:02 +0100

    feat: validate block height
    - fastify pre hook for validation

--- src/routes/blockRoutes/blockRoute.ts
+++ src/routes/blockRoutes/blockRoute.ts
@@ -1,30 +1,56 @@
 import crypto from 'crypto';
-import { type FastifyInstance } from 'fastify';
+import { max } from 'drizzle-orm';
+import { type FastifyInstance, type FastifyRequest } from 'fastify';
+import { blocks } from '../../db/schema';
 import { PostBlockSchema, type PostBlockBody } from './schema/postBlock';
+
+const validateBlock = async (
+  request: FastifyRequest<{ Body: PostBlockBody }>
+) => {
+  const { height, id, transactions } = request.body;
+
+  const transactionIds = transactions
+    .map((transaction) => transaction.id)
+    .join('');
+
+  const blockId = crypto
+    .createHash('sha256')
+    .update(height.toString() + transactionIds)
+    .digest('hex');
+
+  if (blockId !== id) {
+    throw new Error('Invalid block id');
+  }
+
+  const [latestBlock] = await request.server.db
+    .select({
+      recentHeight: max(blocks.height),
+    })
+    .from(blocks)
+    .limit(1);
+
+  if (latestBlock.recentHeight && latestBlock.recentHeight !== height - 1) {
+    throw new Error('Invalid block height');
+  }
+
+  if (!latestBlock.recentHeight && height !== 1) {
+    throw new Error('Invalid block height');
+  }
+};
 
 const blockRoute = async (fastify: FastifyInstance) => {
   fastify.post<{ Body: PostBlockBody }>(
     '/block',
-    { schema: PostBlockSchema },
+    { schema: PostBlockSchema, preHandler: validateBlock },
     async (request, reply) => {
-      const { height, id, transactions } = request.body;
-
-      const transactionIds = transactions
-        .map((transaction) => transaction.id)
-        .join('');
-
-      const blockId = crypto
-        .createHash('sha256')
-        .update(height.toString() + transactionIds)
-        .digest('hex');
-
-      if (blockId !== id) {
-        return reply.status(400).send({ message: 'Invalid block id' });
+      try {
+        return reply
+          .status(201)
+          .send({ message: 'Block created successfully' });
+      } catch (error) {
+        console.error(error);
+        throw error;
       }
-
-      
-
-      return reply.status(201).send({ message: 'Block created successfully' });
     }
   );
 };
