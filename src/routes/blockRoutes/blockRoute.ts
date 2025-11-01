11c27b293c0e25ce37fd70110820ae58c08d9d9b
kshitij <mainalikshitij>
2025-11-01 21:37:45 +0100

    feat: post block controller
    - validate transaction id
    
    - build output table row & user balances
    
    - insert output transaction & user balance as db transaction

--- src/routes/blockRoutes/blockRoute.ts
+++ src/routes/blockRoutes/blockRoute.ts
@@ -1,7 +1,12 @@
 import crypto from 'crypto';
-import { max } from 'drizzle-orm';
+import { and, eq, inArray, max } from 'drizzle-orm';
 import { type FastifyInstance, type FastifyRequest } from 'fastify';
-import { blocks } from '../../db/schema';
+import {
+  balances,
+  blocks,
+  outputs,
+  type OutputTableRow,
+} from '../../db/schema';
 import { PostBlockSchema, type PostBlockBody } from './schema/postBlock';
 
 const validateBlock = async (
@@ -44,6 +49,133 @@ const blockRoute = async (fastify: FastifyInstance) => {
     { schema: PostBlockSchema, preHandler: validateBlock },
     async (request, reply) => {
       try {
+        const { transactions } = request.body;
+
+        const transactionIds = transactions.map(
+          (transaction) => transaction.id
+        );
+
+        const uniqueTransactionIds = [...new Set(transactionIds)];
+
+        if (uniqueTransactionIds.length !== transactionIds.length) {
+          throw new Error('Duplicate transaction ids found in the block');
+        }
+
+        const existingTransactionWithSameIds = await request.server.db
+          .select({
+            txId: outputs.txId,
+          })
+          .from(outputs)
+          .where(inArray(outputs.txId, transactionIds));
+
+        if (existingTransactionWithSameIds.length > 0) {
+          throw new Error(
+            `Transaction with ids ${transactionIds.join(', ')} already exists`
+          );
+        }
+
+        const outputTableRows: Omit<OutputTableRow, 'id' | 'createdAt'>[] = [];
+        const balanceTableRows: Map<string, number> = new Map();
+
+        for (const transaction of transactions) {
+          // if there is no input, then this is a coinbase transaction and txn is valid
+          if (!transaction.inputs.length) {
+            for (const [index, output] of transaction.outputs.entries()) {
+              outputTableRows.push({
+                txId: transaction.id,
+                index,
+                address: output.address,
+                spent: false,
+                blockHeight: request.body.height,
+                amount: output.value.toString(),
+              });
+
+              balanceTableRows.set(
+                output.address,
+                (balanceTableRows.get(output.address) || 0) + output.value
+              );
+            }
+            continue;
+          }
+
+          // if there is an input, then we need to check if the sum of the inputs is equal to the sum of the outputs
+          const existingOutputs = await Promise.all(
+            transaction.inputs.map(async (input) => {
+              const [output] = await request.server.db
+                .select()
+                .from(outputs)
+                .where(
+                  and(
+                    eq(outputs.txId, input.txId),
+                    eq(outputs.index, input.index)
+                  )
+                );
+
+              if (output && output.spent) {
+                throw new Error(
+                  `The transaction with id ${input.txId} and index ${input.index} has already been spent`
+                );
+              }
+
+              if (!output) {
+                throw new Error(
+                  `The transaction with id ${input.txId} and index ${input.index} does not exist`
+                );
+              }
+
+              return output;
+            })
+          );
+
+          const totalInputAmount = existingOutputs.reduce(
+            (acc, output) => acc + parseFloat(output.amount),
+            0
+          );
+
+          const totalOutputAmount = transaction.outputs.reduce(
+            (acc, output) => acc + output.value,
+            0
+          );
+
+          if (totalInputAmount !== totalOutputAmount) {
+            throw new Error(
+              'Total input amount does not match total output amount'
+            );
+          }
+
+          for (const [index, output] of transaction.outputs.entries()) {
+            outputTableRows.push({
+              txId: transaction.id,
+              index,
+              address: output.address,
+              spent: false,
+              blockHeight: request.body.height,
+              amount: output.value.toString(),
+            });
+
+            balanceTableRows.set(
+              output.address,
+              (balanceTableRows.get(output.address) || 0) + output.value
+            );
+          }
+        }
+
+        request.server.db.transaction(async (tx) => {
+          await tx.insert(outputs).values(outputTableRows).execute();
+          await tx
+            .insert(balances)
+            .values(
+              Array.from(balanceTableRows.entries()).map(
+                ([address, balance]) => ({
+                  address,
+                  balance: balance.toString(),
+                  updatedAt: new Date().toISOString(),
+                })
+              )
+            )
+            .execute();
+        });
+
         return reply
           .status(201)
           .send({ message: 'Block created successfully' });
